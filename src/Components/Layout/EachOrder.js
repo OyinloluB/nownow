3d66adb31351f5447b53da85a8cdcab48e08d9a4
Oyinloluwa Balogun <OBalogun@gcn.ab-inbev.com>
2020-06-05 05:54:13 -0700

    .

---
+++ src/Components/Layout/EachOrder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const EachOrder = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/Components/Layout/OrderItem.js
+++ src/Components/Layout/OrderItem.js
@@ -5,7 +5,7 @@ const OrderItem = ({ receivedOrders }) => {
   console.log(receivedOrders);
   return (
     <div>
-      <Card style={{ width: "100%" }}>
+      <Card style={{ width: "100" }}>
         <Card.Body>
           {receivedOrders.map((item) =>
             item ? (
@@ -13,6 +13,7 @@ const OrderItem = ({ receivedOrders }) => {
                 style={{
                   borderBottom: "1px solid rgb(223, 223, 223)",
                   paddingBottom: "9px",
+                  padding: "9px",
                 }}
               >
                 <Col xs={3} md={3} lg={3}>
@@ -22,14 +23,60 @@ const OrderItem = ({ receivedOrders }) => {
                 </Col>
                 <Col xs={3} md={6} lg={6}>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    <h6>{item.items[0].details.brand}</h6>
+                    <ul
+                      style={{
+                        listStyle: "none",
+                        paddingLeft: "0rem",
+                      }}
+                    >
+                      <li>
+                        <b>Unit Price: </b>
+                        {item.items[0].details.price}
+                      </li>
+                      <li>
+                        <b>Quantity: </b>
+                      </li>
+                      <li>
+                        <b>Cost:</b>
+                      </li>
+                      <li>
+                        <b>Placed On: </b>
+                      </li>
+                      <li>
+                        <b>Delivered On: </b>
+                      </li>
+                    </ul>
                   </Card.Text>
                 </Col>
                 <Col xs={3} md={3} lg={3}>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  <Card.Text
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <button
+                      style={{
+                        backgroundColor: "#00FF00",
+                        border: "1px solid #00FF00",
+                        color: "white",
+                        padding: "8px",
+                      }}
+                    >
+                      Accept
+                    </button>
+                    <br />
+                    <button
+                      style={{
+                        backgroundColor: "#b11917",
+                        border: "1px solid #b11917",
+                        color: "white",
+                        padding: "8px",
+                      }}
+                    >
+                      Decline
+                    </button>
                   </Card.Text>
                 </Col>
               </Row>
